7e87330d27d938965f6a689c6e40c9a1e6d7a757
z-for-zefir <alyona.ermak@gmail.com>
2021-12-07 20:32:01 +0300

    remove_element solving

--- src/arrays/deleting_items/remove_element/Main.java
+++ src/arrays/deleting_items/remove_element/Main.java
@@ -87,8 +87,17 @@ public class Main {
     }
 
     public static int removeElement(int[] nums, int val) {
+        int k = nums.length;
 
-        return 0;
+        for (int i = 0; i < k; i++) {
+            if (nums[i] == val) {
+                nums[i] = nums[k - 1];
+                k--;
+                i--;
+            }
+        }
+
+        return k;
     }
 
 }
