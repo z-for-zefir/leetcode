c4e32463ab855c23aeaebf0956064a299a6a1c91
z-for-zefir <alyona.ermak@gmail.com>
2022-03-02 18:06:17 +0300

    Adding move_zeros package and the task in arrays/in_place_operations/move_zeros/Main.java

---
+++ src/arrays/in_place_operations/move_zeros/Main.java
@@ -0,0 +1,46 @@
+package arrays.in_place_operations.move_zeros;
+
+/*
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ * Example 1:
+ *
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ *
+ * Example 2:
+ *
+ * Input: nums = [0]
+ * Output: [0]
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * */
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        /*int[] nums = new int[]{0, 1, 0, 3, 12};
+        int[] expectedNums = new int[]{1, 3, 12, 0, 0};*/
+
+        int[] nums = new int[] {1, 2, 3};
+        int[] expectedNums = new int[] {1, 2, 3};
+
+        moveZeros(nums);
+
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(expectedNums));
+
+
+
+    }
+
+    private static void moveZeros(int[] nums) {
+
+    }
+}
